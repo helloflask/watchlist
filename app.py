8537d98bdd7828b1f7aa2431bbd5a16e757a3cc4
Grey Li <withlihui@gmail.com>
2018-12-16 18:29:40 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 @app.route('/')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
