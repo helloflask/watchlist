c190323316fe3f9999c183b5d938e4d01465d3bd
Grey Li <withlihui@gmail.com>
2018-12-30 16:19:13 +0800

    Add error handler for 400 and 500

--- app.py
+++ app.py
@@ -78,9 +78,19 @@ def inject_user():
     return dict(user=user)
 
 
+@app.errorhandler(400)
+def bad_request(e):
+    return render_template('400.html'), 400
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/', methods=['GET', 'POST'])
