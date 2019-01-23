b79574141d0410c3f2ad6a1f8d3ba05bfe9b3a87
Grey Li <withlihui@gmail.com>
2019-01-23 12:26:36 +0800

    Orignize application with package

--- test_watchlist.py
+++ test_watchlist.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from app import app, db, Movie, User, forge, initdb
+from watchlist import app, db
+from watchlist.models import Movie, User
+from watchlist.commands import forge, initdb
 
 
 class WatchlistTestCase(unittest.TestCase):
