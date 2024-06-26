6d333d7d82177df8a5f568138665b6f0e1a6613d
Frank Yu <withyuxiaoy@gmail.com>
2024-06-27 01:04:02 +0800

    SQLAlchemy v2 code style

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,110 +1,111 @@
-from flask import render_template, request, url_for, redirect, flash
-from flask_login import login_user, login_required, logout_user, current_user
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
 
 from watchlist import app, db
-from watchlist.models import User, Movie
+from watchlist.models import Movie, User
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
+    if request.method == "POST":
         if not current_user.is_authenticated:
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-        title = request.form['title']
-        year = request.form['year']
+        title = request.form["title"]
+        year = request.form["year"]
 
         if not title or not year or len(year) != 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('index'))
+            flash("Invalid input.")
+            return redirect(url_for("index"))
 
         movie = Movie(title=title, year=year)
         db.session.add(movie)
         db.session.commit()
-        flash('Item created.')
-        return redirect(url_for('index'))
+        flash("Item created.")
+        return redirect(url_for("index"))
 
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
+    movies = db.session.scalars(db.select(Movie)).all()
+    return render_template("index.html", movies=movies)
 
 
-@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@app.route("/movie/edit/<int:movie_id>", methods=["GET", "POST"])
 @login_required
 def edit(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
+    movie = db.get_or_404(Movie, movie_id)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
+    if request.method == "POST":
+        title = request.form["title"]
+        year = request.form["year"]
 
         if not title or not year or len(year) != 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('edit', movie_id=movie_id))
+            flash("Invalid input.")
+            return redirect(url_for("edit", movie_id=movie_id))
 
         movie.title = title
         movie.year = year
         db.session.commit()
-        flash('Item updated.')
-        return redirect(url_for('index'))
+        flash("Item updated.")
+        return redirect(url_for("index"))
 
-    return render_template('edit.html', movie=movie)
+    return render_template("edit.html", movie=movie)
 
 
-@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@app.route("/movie/delete/<int:movie_id>", methods=["POST"])
 @login_required
 def delete(movie_id):
-    movie = Movie.query.get_or_404(movie_id)
+    movie = db.get_or_404(Movie, movie_id)
     db.session.delete(movie)
     db.session.commit()
-    flash('Item deleted.')
-    return redirect(url_for('index'))
+    flash("Item deleted.")
+    return redirect(url_for("index"))
 
 
-@app.route('/settings', methods=['GET', 'POST'])
+@app.route("/settings", methods=["GET", "POST"])
 @login_required
 def settings():
-    if request.method == 'POST':
-        name = request.form['name']
+    if request.method == "POST":
+        name = request.form["name"]
 
         if not name or len(name) > 20:
-            flash('Invalid input.')
-            return redirect(url_for('settings'))
+            flash("Invalid input.")
+            return redirect(url_for("settings"))
 
-        user = User.query.first()
+        user = db.first_or_404(db.select(User))
+
         user.name = name
         db.session.commit()
-        flash('Settings updated.')
-        return redirect(url_for('index'))
+        flash("Settings updated.")
+        return redirect(url_for("index"))
 
-    return render_template('settings.html')
+    return render_template("settings.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
         if not username or not password:
-            flash('Invalid input.')
-            return redirect(url_for('login'))
+            flash("Invalid input.")
+            return redirect(url_for("login"))
 
-        user = User.query.first()
+        user = db.first_or_404(db.select(User))
 
         if username == user.username and user.validate_password(password):
             login_user(user)
-            flash('Login success.')
-            return redirect(url_for('index'))
+            flash("Login success.")
+            return redirect(url_for("index"))
 
-        flash('Invalid username or password.')
-        return redirect(url_for('login'))
+        flash("Invalid username or password.")
+        return redirect(url_for("login"))
 
-    return render_template('login.html')
+    return render_template("login.html")
 
 
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    flash('Goodbye.')
-    return redirect(url_for('index'))
+    flash("Goodbye.")
+    return redirect(url_for("index"))
